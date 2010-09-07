3497f3fab4c558bd2ca857b864169fc0bd8cc5da
simon.foster <simon.foster@eurosafeuk.co.uk>
2010-09-07 16:40:53 +0100

    removed log files

--- trunk/download/count.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include $_SERVER["DOCUMENT_ROOT"].'/db/dbdownload.php';
-	$ans = updatecount($_GET['id']);
-	$host  = $_SERVER['HTTP_HOST'];
-	$id=$_GET['id'];
-	header("Location: http://$host/download/$id");
-?>
--- trunk/gall/250.php
+++
@@ -1,5 +0,0 @@
-<?php
-	# test.php
-	include $_SERVER["DOCUMENT_ROOT"].'/gall/image_resize.php'; 
-	image_resize(250,250,'250');
-?> 
